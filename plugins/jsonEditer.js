a3438c2e592c9c5e949172d604c4da18ba0e7707
rajbhojani77 <raj@improwised.com>
2021-07-28 17:44:55 +0530

    changed plugin

--- plugins/jsonEditer.js
+++ plugins/jsonEditer.js
@@ -1,7 +1,3 @@
 import Vue from 'vue'
 import VJsoneditor from 'v-jsoneditor'
-import { BootstrapVue, BootstrapVueIcons } from 'bootstrap-vue'
-
-Vue.use(BootstrapVue)
-Vue.use(BootstrapVueIcons)
 Vue.use(VJsoneditor)
